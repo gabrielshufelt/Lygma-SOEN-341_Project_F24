ae86cfdfb4dfb29ed5f30e72ffeddeb1c93e0613
Luqman1008 <hakimluqman070@gmail.com>
2024-09-21 16:54:06 -0400

    Add Project model with essential fields

---
+++ source/app/models/project.rb
@@ -0,0 +1,5 @@
+class Project < ApplicationRecord
+  # TODO: creating associations for team and instructor models (Do the same in team and instructor models as well)
+  # belongs_to: team
+  # belongs_to: instructor
+end
