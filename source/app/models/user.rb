fe5bf80e3801a17a34540a242450829341aae5f6
Gabriel Shufelt <gabshu2014@gmail.com>
2024-09-22 00:38:18 -0400

    New Sign Up Form

--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_save :validate_role
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true
+  validates :role, inclusion: ["student", "instructor"], presence: true
+
+  def student?
+    role == "student"
+  end
+
+  def instructor?
+    role == "instructor"
+  end
+
+  def validate_role
+    if instructor?      # instructors cannot belong to a team, nor have a rating
+      self.team_id = nil
+      self.cooperation_rating = nil
+      self.conceptual_rating = nil
+      self.practical_rating = nil
+      self.work_ethic_rating = nil
+    end
+  end
 end
