eb98dc82d5c51dc71f1f5a6642ce15ddc5e09814
Luqman1008 <hakimluqman070@gmail.com>
2024-09-22 08:39:08 -0400

    Add validations to Project model and test cases

--- source/app/models/project.rb
+++ source/app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ApplicationRecord
+  # Validations
+  validates :title, presence: true
+  validates :team_id, presence: true
+  validates :due_date, presence: true
+  validates :instructor_id, presence: true
+
   # TODO: creating associations for team and instructor models (Do the same in team and instructor models as well)
   # belongs_to: team
   # belongs_to: instructor
