aa265036bcef803bd01793a7107d6e1e24e9119d
Gabriel Shufelt <gabshu2014@gmail.com>
2024-10-21 00:33:51 -0400

    Student Progression Chart complete

--- source/app/controllers/student_dashboard_controller.rb
+++ source/app/controllers/student_dashboard_controller.rb
@@ -4,23 +4,20 @@ class StudentDashboardController < ApplicationController
   def index
     @upcoming_evaluations = upcoming_evaluations
     @avg_ratings = avg_ratings
-    # render json: { upcoming_evaluations: @upcoming_evaluations, avg_ratings: @avg_ratings }
+    @student_evaluations_progression = student_evaluations_progression
   end
 
   def teams
     @student_teams = student_teams
-    # render json: @student_teams
   end
 
   def evaluations
     @student_evaluations = student_evaluations
-    # render json: @student_evaluations
   end
 
   def feedback
     @avg_ratings = avg_ratings
     @received_evaluations = received_evaluations
-    # render json: { avg_ratings: @avg_ratings, received_evaluations: @received_evaluations }
   end
 
   private
@@ -29,80 +26,62 @@ class StudentDashboardController < ApplicationController
     @student = current_user if current_user.role == 'student'
   end
 
-
   def upcoming_evaluations
-    begin
-      evaluations = Evaluation.joins(:project).where(evaluator_id: @student.id, status: 'pending')
-      evaluations || [] # Return empty array if no evaluations found
-    rescue => e
-      Rails.logger.error "Error fetching upcoming evaluations: #{e.message}"
-      [] # Return an empty array to handle the error
-    end
+    evaluations = Evaluation.joins(:project).where(evaluator_id: @student.id, status: 'pending')
+    evaluations || [] # Return empty array if no evaluations found
   end
-  
 
   def avg_ratings
-    begin
-      completed_evaluations = Evaluation.where(evaluatee_id: @student.id, status: 'completed')
-      {
-        conceptual: completed_evaluations.average(:conceptual_rating) || 0.0,
-        cooperation: completed_evaluations.average(:cooperation_rating) || 0.0,
-        practical: completed_evaluations.average(:practical_rating) || 0.0,
-        work_ethic: completed_evaluations.average(:work_ethic_rating) || 0.0
-      }
-    rescue => e
-      Rails.logger.error "Error calculating average ratings: #{e.message}"
-      { conceptual: 0.0, cooperation: 0.0, practical: 0.0, work_ethic: 0.0 } # Default to 0.0 if error occurs
-    end
+    completed_evaluations = Evaluation.where(evaluatee_id: @student.id, status: 'completed')
+    {
+      conceptual: completed_evaluations.average(:conceptual_rating).round(2) || 0.0,
+      cooperation: completed_evaluations.average(:cooperation_rating).round(2) || 0.0,
+      practical: completed_evaluations.average(:practical_rating).round(2) || 0.0,
+      work_ethic: completed_evaluations.average(:work_ethic_rating).round(2) || 0.0
+    }
   end
 
-
   def student_teams
-    begin
-      projects = @student.courses.map(&:projects).flatten
-      return [] if projects.empty? # Return empty array if no projects are found
+    projects = @student.courses.map(&:projects).flatten
+    return [] if projects.empty? # Return empty array if no projects are found
 
-      projects.map do |project|
-        {
-          project_id: project.id,
-          student_team: @student.teams.where(project_id: project.id),
-          all_teams: project.teams
-        }
-      end
-    rescue => e
-      Rails.logger.error "Error fetching student teams: #{e.message}"
-      [] # Return an empty array to handle the error
+    projects.map do |project|
+      {
+        project_id: project.id,
+        student_team: @student.teams.where(project_id: project.id),
+        all_teams: project.teams
+      }
     end
   end
 
   def student_evaluations
-    begin
-      projects = @student.courses.map(&:projects).flatten
-      return [] if projects.empty? # Return empty array if no projects are found
+    projects = @student.courses.map(&:projects).flatten
+    return [] if projects.empty? # Return empty array if no projects are found
 
-      projects.map do |project|
-        {
-          project_id: project.id,
-          due_date: project.due_date,
-          completed: Evaluation.where(evaluator_id: @student.id, project_id: project.id, status: 'completed'),
-          pending: Evaluation.where(evaluator_id: @student.id, project_id: project.id, status: 'pending')
-        }
-      end
-    rescue => e
-      Rails.logger.error "Error fetching student evaluations: #{e.message}"
-      [] # Return an empty array to handle the error
+    projects.map do |project|
+      {
+        project_id: project.id,
+        due_date: project.due_date,
+        completed: Evaluation.where(evaluator_id: @student.id, project_id: project.id, status: 'completed'),
+        pending: Evaluation.where(evaluator_id: @student.id, project_id: project.id, status: 'pending')
+      }
     end
   end
 
-
   def received_evaluations
-    begin
-      evaluations = Evaluation.where(evaluatee_id: @student.id, status: 'completed')
-      evaluations || [] # Return empty array if no evaluations found
-    rescue => e
-      Rails.logger.error "Error fetching received evaluations: #{e.message}"
-      [] # Return an empty array to handle the error
-    end
-  end  
+    evaluations = Evaluation.where(evaluatee_id: @student.id, status: 'completed')
+    evaluations || [] # Return empty array if no evaluations found
+  end
 
+  def student_evaluations_progression
+    evaluations = Evaluation.where(status: 'completed')
+                            .where(evaluatee_id: 2)
+                            .group('DATE(date_completed)')
+                            .select('DATE(date_completed) as date_completed,
+                                    AVG(cooperation_rating) as avg_cooperation,
+                                    AVG(conceptual_rating) as avg_conceptual,
+                                    AVG(practical_rating) as avg_practical,
+                                    AVG(work_ethic_rating) as avg_work_ethic')
+    evaluations || []
+  end
 end
