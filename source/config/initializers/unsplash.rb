7bd70820dfe486775592da1f4c54e1e78a83d49b
Luqman1008 <hakimluqman070@gmail.com>
2024-10-23 02:28:07 -0400

    Added better image generation tool :)

--- source/config/initializers/unsplash.rb
+++
@@ -1,7 +0,0 @@
-require 'unsplash'
-
-Unsplash.configure do |config|
-  config.application_access_key = 'ygEqNVA9ZpUe2cazRa1QWzvvPxcvVwedF9pQDHWCuhk'
-  config.application_secret = 'DaagMEyfFvKahk8EbGqCguBvg8wVTMb2bqpHrsR-VlE'
-  config.utm_source = "Real Ratings"
-end
