dda28bfc007a26b6d9ac9920979d1640c2de6743
Ahmad <ahmad.saadawi7@gmail.com>
2024-11-11 00:28:16 -0500

    fixed registrations_controller.rb

--- source/app/controllers/users/registrations_controller.rb
+++ source/app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role, :sex ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role, :sex, :student_id ])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :role, :cooperation_rating, :conceptual_rating, :practical_rating, :work_ethic_rating, :sex])
   end
 end
